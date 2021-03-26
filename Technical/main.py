df599bd6f9bf28fa90eaba8a56b4debba8cfdfb7
Abanoub-G <ag14622@bristol.ac.uk>
2021-03-26 09:56:44 +0000

    just pushing some chanes done to the main code

--- Technical/main.py
+++ Technical/main.py
@@ -15,7 +15,7 @@ import flowiz as fz
 #   pass
 # return flownet_image
 
-def convert_optical_flow_to_reduced_veolcity_plot(flownet_image_file_name, res_x, res_y):
+def reduce_optical_flow_resolution(flownet_image_file_name, res_x, res_y):
   files = glob.glob(flownet_image_file_name)
   floArray = fz.read_flow(files[0])
   uv = fz.convert_from_flow(floArray, mode='UV')
@@ -194,28 +194,34 @@ def yolo_extract_objects_in_image(image_path):
   return boxes
 
 
-# class feature_vector():
-#   def __init__(self,w1,w2,w3,optical_flow_resultant, box, confidences):
-#     self.velocity_mean_w1       =  * w1
-#     self.velocity_variance_w1   =  * w1
-#     self.velocity_skewness_w1   =  * w1
-#     self.velocity_kurtosis_w1   =  * w1
-#     self.C_x_w2                 =  * w2
-#     self.C_y_w2                 =  * w2
-#     self.area_w2                =  * w2
+class feature_vector():
+  def __init__(self,w1,w2,w3,optical_flow_resultant, box, confidence):
+    self.velocity_mean_w1       =  * w1
+    self.velocity_variance_w1   =  * w1
+    self.velocity_skewness_w1   =  * w1
+    self.velocity_kurtosis_w1   =  * w1
+    self.C_x_w2                 =  * w2
+    self.C_y_w2                 =  * w2
+    self.area_w2                =  * w2
 
-#     self.classes_confidences_w3 = confidences * w3
-
-
+    self.classes_confidences_w3 = confidence * w3
 
 
 
-    
+
+
+def extract_optical_flow_section_of_detected_object(original_image, boxes, optical_flow_resultant):
+  
+
 
 # def produce_feature_vector(optical_flow_image, boxes_of_detected_objects, ):
 
 
 # def map_yolo_to optical_flow():
-yolo_extract_objects_in_image("180.jpg")
 
-# convert_optical_flow_to_reduced_veolcity_plot('180.flo', 10, 10)
+
+
+
+# yolo_extract_objects_in_image("180.jpg")
+
+# reduce_optical_flow_resolution('180.flo', 10, 10)
