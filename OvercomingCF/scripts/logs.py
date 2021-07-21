a0d1cced065c5fb828b609df50ab8b4fd866bf91
Abanoub-G <ag14622@bristol.ac.uk>
2021-07-21 23:25:31 +0100

    Added results for varying initial training dataset

--- OvercomingCF/scripts/logs.py
+++ OvercomingCF/scripts/logs.py
@@ -2,25 +2,27 @@ import os
 
 class logs():
 	def __init__(self):
-		self.retraining_dataset_no_array           = []
+		self.current_training_dataset_no_array     = []
+		self.current_training_dataset_size_array   = []
 		self.ewc_lambda_array                      = []
 		self.lr_init_array                         = []
 		self.momentum_init_array                   = []
 		self.lr_cont_array                         = []
 		self.momentum_cont_array                   = []
-		self.dataset_no_array                      = []
-		self.dataset_size_array                    = []
+		self.evaluation_dataset_no_array           = []
+		self.evaluation_dataset_size_array         = []
 		self.acc_array                             = []
 
-	def append(self, retraining_dataset_no, ewc_lambda, lr_init, momentum_init, lr_cont, momentum_cont, dataset_no, dataset_size, acc):
-		self.retraining_dataset_no_array.append(retraining_dataset_no)
+	def append(self, current_training_dataset_no, current_training_dataset_size, ewc_lambda, lr_init, momentum_init, lr_cont, momentum_cont, evaluation_dataset_no, evaluation_dataset_size, acc):
+		self.current_training_dataset_no_array.append(current_training_dataset_no)
+		self.current_training_dataset_size_array.append(current_training_dataset_size)
 		self.ewc_lambda_array.append(ewc_lambda)
 		self.lr_init_array.append(lr_init)
 		self.momentum_init_array.append(momentum_init)
 		self.lr_cont_array.append(lr_cont)
 		self.momentum_cont_array.append(momentum_cont)
-		self.dataset_no_array.append(dataset_no)
-		self.dataset_size_array.append(dataset_size)
+		self.evaluation_dataset_no_array.append(evaluation_dataset_no)
+		self.evaluation_dataset_size_array.append(evaluation_dataset_size)
 		self.acc_array.append(acc)
 
 	def write_file(self, file_name):
@@ -31,10 +33,10 @@ class logs():
 
 		file_path = os.path.join(output_folder, file_name)
 		with open(file_path, 'w') as log_file: 
-			log_file.write('retraining_dataset_no, ewc_lambda, lr_init, momentum_init, lr_cont, momentum_cont, dataset_no, dataset_size, acc\n')
-			for i in range(len(self.retraining_dataset_no_array)):
-				log_file.write('%d, %3.6f, %3.6f, %3.6f, %3.6f, %3.6f, %d, %d, %3.6f\n' %\
-					(self.retraining_dataset_no_array[i],self.ewc_lambda_array[i], self.lr_init_array[i], self.momentum_init_array[i], self.lr_cont_array[i], self.momentum_cont_array[i], self.dataset_no_array[i], self.dataset_size_array[i], self.acc_array[i]))
+			log_file.write('current_training_dataset_no, current_training_dataset_size, ewc_lambda, lr_init, momentum_init, lr_cont, momentum_cont, evaluation_dataset_no, evaluation_dataset_size, acc\n')
+			for i in range(len(self.current_training_dataset_no_array)):
+				log_file.write('%d, %d, %3.6f, %3.6f, %3.6f, %3.6f, %3.6f, %d, %d, %3.6f\n' %\
+					(self.current_training_dataset_no_array[i], self.current_training_dataset_size_array[i],self.ewc_lambda_array[i], self.lr_init_array[i], self.momentum_init_array[i], self.lr_cont_array[i], self.momentum_cont_array[i], self.evaluation_dataset_no_array[i], self.evaluation_dataset_size_array[i], self.acc_array[i]))
 		print('Log file SUCCESSFULLY generated!')
 
 
